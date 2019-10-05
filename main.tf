b8ac5cb3522d32af1bc71536d81d09d40273e5c4
narlacloud <55445733+narlacloud@users.noreply.github.com>
2019-10-05 22:31:45 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -100,7 +100,7 @@ resource "aws_instance" "web-1" {
     ami = "${data.aws_ami.my_ami.id}"
     availability_zone = "us-east-1a"
     instance_type = "t2.nano"
-    key_name = "LaptopKey"
+    key_name = "root_pub_key"
     subnet_id = "${aws_subnet.subnet1-public.id}"
     vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
     associate_public_ip_address = true	
